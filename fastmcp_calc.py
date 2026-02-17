ff6638852594926269b6c198152eb6152cf8c63a
Abdelrahman1-11-2003 <abdelrahmanradaideh.dev@gmail.com>
2026-02-17 09:00:58 +0300

    Update fastmcp_calc.py

--- fastmcp_calc.py
+++ fastmcp_calc.py
@@ -51,5 +51,5 @@ def divide(a: float, b: float) -> float:
 if __name__ == '__main__':
 
     #mcp.run()#STDIO by defualt
-    mport = int(os.environ.get("PORT", 8002))
+    port = int(os.environ.get("PORT", 8002))
     mcp.run(transport="http", host="0.0.0.0", port=port)
